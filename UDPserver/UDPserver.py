6035e98ac7cea75a5361d0c34fed39e8c88d8ac7
Alick-Li <alickli@outlook.com>
2025-05-26 17:10:28 +0800

    2nd

--- UDPserver/UDPserver.py
+++ UDPserver/UDPserver.py
@@ -2,3 +2,24 @@ import socket
 import base64
 import threading
 
+def send_and_receive(socket, address, port, message):
+    timeout = 1000
+    retry_count = 0
+
+    while retry_count < 5:
+        socket.settimeout(timeout / 1000)
+        socket.sendto(message.encode(), (address, port))
+        response, _ = socket.recvfrom(2048)
+        return response.decode()
+    
+    return None
+
+def download_file(socket, address, port, filename):
+    response = send_and_receive(socket, address, port, f"DOWNLOAD {filename}")
+
+    if response.startswith("OK"):
+        file_size = int(response.split()[3])
+        data_port = int(response.split()[5])
+
+        return True
+    
